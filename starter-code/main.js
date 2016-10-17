9b29cded2810d5facdf2f63f034a41d2601b75c6
Daniel A Normandia <danielanormandia@Daniels-MacBook-Air.local>
2016-10-16 22:24:41 -0400

    Added If Else Statement

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,17 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+//card value if statement
+if (cardTwo === cardFour) {
+	alert('Sorry, try again.');
+}else if (cardTwo === cardThree) {
+	alert('Sorry, try again.');
+}else if (cardTwo === cardOne) {
+	alert('You found a match!');
+}else if (cardOne === cardThree) {
+	alert('Sorry, try again.');
+}else if (cardOne === cardFour) {
+	alert('Sorry, try again.');
+}else  {										//(cardThree === cardFour)
+	alert('You found a match!');
+}
